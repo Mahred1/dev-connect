cb4ffe6317c9a572bd50e220fd5add783da1212b
Mahred1 <mredwan.kemal@gmail.com>
2025-11-08 11:03:58 +0300

    implemented github Oauth

--- components/Form/SocialForm.tsx
+++ components/Form/SocialForm.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client"
+import { signIn } from "next-auth/react";
 import { Button } from "../ui/button";
 import { FaGithub } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
@@ -6,7 +7,7 @@ import { FcGoogle } from "react-icons/fc";
 const SocialForm = () => {
   return (
     <div className="mt-10 flex gap-2.5">
-      <Button className="bg-gray-900 min-h-12 flex-1 py-3.5 cursor-pointer ">
+      <Button onClick={()=>signIn("github",{callbackUrl:"/"})} className="bg-gray-900 min-h-12 flex-1 py-3.5 cursor-pointer ">
         <FaGithub style={{width:'24px',height:'24px'}} />
         <span className="text-md ">Login with GitHub</span>
       </Button>

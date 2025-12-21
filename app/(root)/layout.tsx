3499baa326ddaa376040962898cd466552895ec0
Mahred1 <mredwan.kemal@gmail.com>
2025-12-21 17:44:36 +0300

    fixed root layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import NavBar from '../../components/navigation/navbar/NavBar'
-import MobileSideBar from '@/components/navigation/sidebar/MobileSideBar'
-import SideBar from '@/components/navigation/sidebar/SideBar'
+import React from "react";
+import NavBar from "../../components/navigation/navbar/NavBar";
+import MobileSideBar from "@/components/navigation/sidebar/MobileSideBar";
+import SideBar from "@/components/navigation/sidebar/SideBar";
 
-const RootLayout = ({children}:{children:React.ReactNode}) => {
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main>
+      <NavBar />
 
-        <NavBar/>
-        <MobileSideBar/>
-        <div className='flex '>
-          <SideBar/>
-        {children}
-        </div>
-        
+      <MobileSideBar />
+      <div className="max-w-screen flex">
+        <SideBar />
+        <div className="flex-1 p-5">{children}</div>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;

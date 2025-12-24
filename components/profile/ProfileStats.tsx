3781f49f5947b4d1d5146c113fb0120a0b0786db
Mahred1 <mredwan.kemal@gmail.com>
2025-12-24 22:17:39 +0300

    re style stat card

--- components/profile/ProfileStats.tsx
+++ components/profile/ProfileStats.tsx
@@ -1,21 +1,40 @@
-import Image from 'next/image'
-import React from 'react'
-import StatCard from './StatCard'
+import Image from "next/image";
+import React from "react";
+import StatCard from "./StatCard";
 
-const ProfileStats = ({id}:{id:string}) => {
+const ProfileStats = ({ id }: { id: string }) => {
   return (
-    <div className='bg-slate-50 rounded-2xl border-2 border-slate-300 px-3  py-4 w-[350px]'>
-        <span className='text-xl'>Statistics</span>
+    <div className="bg-slate-50 rounded-2xl border-2 border-slate-300 px-4  pt-6 pb-4 w-[350px]">
+      <span className="text-xl">Statistics</span>
 
-        <div className='mt-3'>
-           
-            <StatCard image={"/icons/jobs.svg"} heading="Profile View" value={1245}/>
-            <StatCard image={"/icons/projects.svg"} heading="Projects" value={22}/>
-            <StatCard image={"/icons/network.svg"} heading="Connections" value={125}/>
-            <StatCard image={"/icons/bell.svg"} heading="Achievments" value={89}/>
-        </div>
+      <div className="mt-8">
+        <StatCard
+          image={"/icons/jobs.svg"}
+          heading="Profile View"
+          value={1245}
+          color={"red"}
+        />
+        <StatCard
+          image={"/icons/projects.svg"}
+          heading="Projects"
+          value={22}
+          color={"green"}
+        />
+        <StatCard
+          image={"/icons/network.svg"}
+          heading="Connections"
+          value={125}
+          color={"purple"}
+        />
+        <StatCard
+          image={"/icons/bell.svg"}
+          heading="Achievments"
+          value={89}
+          color={"orange"}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProfileStats
+export default ProfileStats;
--- components/profile/StatCard.tsx
+++ components/profile/StatCard.tsx
@@ -1,19 +1,33 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
-const StatCard = ({image,heading,value}:{image:string,heading:string,value:number}) => {
+const StatCard = ({
+  image,
+  heading,
+  value,
+  color
+}: {
+  image: string;
+  heading: string;
+  value: number;
+  color:string
+}) => {
   return (
-     <div className='flex gap-3  items-center mb-4'>
-                <div className='relative w-10 h-10'>
-                <Image className='bg-red-100  rounded-md p-2'  src={image} fill alt="Profile View" />
+    <div className="flex gap-3  items-center mb-4">
+      <div className="relative w-10 h-10">
+        <Image
+          className={`bg-${color}-100  rounded-md p-2`}
+          src={image}
+          fill
+          alt="Profile View"
+        />
+      </div>
+      <div className="flex flex-col">
+        <span className="text-slate-600">{heading}</span>
+        <span className="text-md">{value}</span>
+      </div>
+    </div>
+  );
+};
 
-                </div>
-                <div className='flex flex-col'>
-                    <span className='text-slate-600'>{heading}</span>
-                    <span className='text-md'>{value}</span>
-                </div>
-            </div>
-  )
-}
-
-export default StatCard
+export default StatCard;
